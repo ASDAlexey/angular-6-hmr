32570e2b4e11b770730704eb716639de4529bd31
Alexey <asdalexey@yandex.ru>
2018-05-27 22:29:52 +0300

    add @env folder prefix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component } from '@angular/core';
+import { environment } from '@env/environment';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component, ViewEncapsulation } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor() {
+    console.log(environment);
+  }
 }
