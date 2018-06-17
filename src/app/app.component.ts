6e1aff459101034f864ef5dd763941ecae55fcbb
Alexey <asdalexey@yandex.ru>
2018-06-18 00:08:24 +0300

    ng serve less mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { environment } from '@env/environment';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.less'],
 })
 export class AppComponent {
   title = 'app';
